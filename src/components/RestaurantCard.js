cc55a82795a1f0a00dc8791aa499bbecf016f281
soumya Dey <soumydey@cisco.com>
2024-09-13 16:44:24 +0530

    added utils dir & refactor

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,5 +1,4 @@
-const THUMBNAIL_IMAGE_BASE_URL =
-  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+import { THUMBNAIL_IMAGE_BASE_URL } from "../utils/constants";
 
 const RestaurantCard = (props) => {
   const {
@@ -15,14 +14,14 @@ const RestaurantCard = (props) => {
 
   const { link } = props;
   return (
-    <div className="res-card" onClick={() => window.open(link, '_blank')}>
+    <div className="res-card" onClick={() => window.open(link, "_blank")}>
       <img
         src={`${THUMBNAIL_IMAGE_BASE_URL}${cloudinaryImageId}`}
         className="res-logo"
       />
       <h3>{name}</h3>
       <h4>
-      ⭐ {avgRating} : <span>{sla?.slaString}</span>
+        ⭐ {avgRating} : <span>{sla?.slaString}</span>
       </h4>
       <h4>{costForTwo}</h4>
       <p>{cuisines?.join(", ")}</p>
