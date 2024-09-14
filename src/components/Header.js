8c6729a8acc4695fab8f7829d7e581769d5de172
soumya Dey <soumydey@cisco.com>
2024-09-14 11:29:29 +0530

    sign-in sign-out to understand useState better

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
+import { SIGN_IN, SIGN_OUT } from "../utils/constants";
 
 const Header = () => {
+  let [btnName, setBtnName] = useState(SIGN_IN);
+
+  const updateBtnName = () => {
+    btnName === SIGN_IN ? setBtnName(SIGN_OUT) : setBtnName(SIGN_IN);
+  };
+
   return (
     <div className="header">
       <div className="logoContainer">
@@ -10,8 +18,10 @@ const Header = () => {
         <ul>
           <li key={"home"}>Home</li>
           <li key={"about"}>About</li>
-          <li key={"signin"}>Sign In</li>
           <li key={"cart"}>Cart</li>
+          <button className="signin" onClick={() => updateBtnName()}>
+            {btnName}
+          </button>
         </ul>
       </div>
     </div>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,8 @@
-export const LOGO_URL = "https://media.istockphoto.com/id/1435983029/vector/food-delivery-logo-images.jpg?s=612x612&w=0&k=20&c=HXPxcjOxUiW4pMW1u9E0k2dJYQOU37a_0qZAy3so8fY=";
+export const LOGO_URL =
+  "https://media.istockphoto.com/id/1435983029/vector/food-delivery-logo-images.jpg?s=612x612&w=0&k=20&c=HXPxcjOxUiW4pMW1u9E0k2dJYQOU37a_0qZAy3so8fY=";
 
-export const THUMBNAIL_IMAGE_BASE_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const THUMBNAIL_IMAGE_BASE_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const SIGN_IN = "Sign In";
+export const SIGN_OUT = "Sign Out";
