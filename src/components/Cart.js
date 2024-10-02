3bf05e227faf3adc38dcabd8e1d9b33f01dd340d
soumya Dey <soumydey@cisco.com>
2024-10-02 21:37:04 +0530

    Implement clearCart button

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,25 +1,29 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import styles from "../styles/Cart.module.css";
 import MenuCategoryItemListAccr from "./MenuCategoryItemListAccr";
+import { clearCart } from "../Store/cartSlice";
 
 const Cart = () => {
   const cartItems = useSelector((store) => store.cart.items);
-  // const itemFrequency = {};
-  // cartItems?.forEach(item => {
-  //   itemFrequency[item?.id] = itemFrequency[item?.id] ? itemFrequency[item?.id] + 1 : 1;
-  // })
-  // const uniqueCartItems = cartItems.filter((value, index, self) =>
-  //   index === self.findIndex((t) => t.id === value.id)
-  // );
+  const dispatch = useDispatch();
 
-  // const items = uniqueCartItems.map(item => ({...item, selectedCount: itemFrequency[item?.id]}));
+  const handleClear = () => {
+    dispatch(clearCart(cartItems));
+  };
 
   return (
     <div className={styles.cart}>
       <h1>Checkout</h1>
-      <div className={styles.cartItems}>
-        <MenuCategoryItemListAccr items={cartItems} />
-      </div>
+      <button className={styles.clearCartBtn} onClick={() => handleClear()}>
+        Clear Cart
+      </button>
+      {cartItems.length > 0 ? (
+        <div className={styles.cartItems}>
+          <MenuCategoryItemListAccr items={cartItems} />
+        </div>
+      ) : (
+        <h3>Cart is empty, please add items</h3>
+      )}
     </div>
   );
 };
